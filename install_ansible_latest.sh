3bb2c4e429d188c99c20f7de498af89ee5d963ad
zliu <zhang.liu87@gmail.com>
2018-10-02 10:18:30 -0600

    update install ansible automation

--- install_ansible_latest.sh
+++ install_ansible_latest.sh
@@ -1,5 +1,5 @@
 sudo apt update
-sudo apt install software-properties-common
-sudo apt-add-repository ppa:ansible/ansible
+sudo apt install software-properties-common -y
+sudo apt-add-repository ppa:ansible/ansible -y
 sudo apt update
-sudo apt install ansible
+sudo apt install ansible -y
